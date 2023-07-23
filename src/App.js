8c4e409b6b0dada092c2ad2cce609aeed99604b0
Samyak8303 <pixelpirate404@gmail.com>
2023-07-23 12:45:50 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css"
-function App() { 
-    return (  
-        <div classNmae = "App">
-            Subscribe Zainkeepscode
-            </div>
-);
+import Auth from "./pages/Auth/Auth";
+import Home from "./pages/home/Home";
+import Profile from "./pages/Profile/Profile";
+function App() {
+  return (
+    <div className="App">
+        <div className="blur" style={{top: '-18%', right: '0'}}></div>
+        <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
+        {/* <Home/> */}
+        <Profile/>
+        {/* <Auth/> */}
+    </div>
+  );
 }
+
 export default App;
