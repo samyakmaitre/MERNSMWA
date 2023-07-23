0c87576256a6c66933ba9061951bb03e61ed4ad4
Samyak8303 <pixelpirate404@gmail.com>
2023-07-23 13:02:50 +0530

    spcae

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/Profile/Profile";
 function App() {
   return (
-    
+
     <div className="App">
         <div className="blur" style={{top: '-18%', right: '0'}}></div>
         <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
@@ -13,6 +13,7 @@ function App() {
         {/* <Auth/> */}
     </div>
   );
+  
 }
 
 export default App;
