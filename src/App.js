d4de449644e7ecb9ed1e2ee994c4de6415eabbf3
Samyak Maitre <pixelpirate404@gmail.com>
2023-08-04 22:14:33 +0530

    space

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Profile from "./pages/Profile/Profile";
 function App() {
   return (
     <div className="App">
+      
         <div className="blur" style={{top: '-18%', right: '0'}}></div>
         <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
         {/* <Home/> */}
